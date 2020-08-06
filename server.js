acc468872ab1e3f66a7da9ce9fded03f55b6d4e4
Ammon Jeffery <savingphace@gmail.com>
2020-08-05 22:11:38 -0600

    made routes to css, js + added requirements

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs")
+const util = require("util")
 
 let app = express();
-let PORT = 8080;
+
+
+let PORT = process.env.PORT || 3000;
+
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 
+//functions to make things go
+
 // Routes
 //-----------------------
+app.get("/notes", function (req,res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+});
+
+app.get("/assets/js/index.js", function (req,res) {
+    res.sendFile(path.join(__dirname, "./assets/js/index.js"))
+});
+
+app.get("/assets/css/styles.css", function (req, res) {
+    res.sendFile(path.join(__dirname, "./assets/css/styles.css"))
+});
+
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html" ))
+})
