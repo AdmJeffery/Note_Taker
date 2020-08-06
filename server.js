3e6def24278b15c2ac1c9a93eda2c8ce92679a4b
Ammon Jeffery <savingphace@gmail.com>
2020-08-05 21:29:36 -0600

    Initial requirements/setup on server done, beginning routes

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-let express = require("express");
-let path = require("path");
+const express = require("express");
+const path = require("path");
+const fs = require("fs")
 
 let app = express();
-let PORT = 3000;
+let PORT = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
+
+// Routes
+//-----------------------
